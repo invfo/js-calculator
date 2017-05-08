7605ae6ff336b4262f30bb28d99ca64ec1b3b17a
Darya Talanina <dtalanina@gmail.com>
2017-05-08 13:00:05 +0200

    First commit: basic html, css and empty tests

---
+++ test_functional.py
@@ -0,0 +1,11 @@
+class TestCalculator():
+    
+    def setup(self):
+        print 'setting up'
+
+    def test_first(self):
+        print 'first test'
+        
+    def test_second(self):
+        print 'second test'
+    
